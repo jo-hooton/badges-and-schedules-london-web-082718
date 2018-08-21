4aaedea0de49d393f4fe6dcb673aa248a5030cea
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-21 21:31:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,39 @@
-# Write your code here.
+names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(attenees)
+  attendee_list = []
+  attendees.each do |name|
+    attendee_list.push("Hello, my name is #{name}.")
+  end
+  return attendee_list
+end
+
+
+
+def assign_rooms(array)
+  nuarray = []
+  counter = 1
+  array.each do |name|
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return nuarray
+end
+
+# assign_rooms(people)
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end 
